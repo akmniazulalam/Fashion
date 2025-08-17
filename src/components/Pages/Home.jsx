0b871bb87594367c018e4e608ba99007d9fccd09
Niaz <niazulalam097@gmail.com>
2025-08-17 23:39:16 +0600

    After added

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -80,9 +80,9 @@ const Home = () => {
                             <Paragraph className={"font-pops font-semibold text-2xl pl-[50px] pt-[50px] pr-10"} pText={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu, odio pellentesque mattis pulvinar felis. At arcu ornare rhoncus, elementum non viverra. "}></Paragraph>
                         </div>
                     </Flex>
-                    <div className='w-[750px] h-[250px] bg-recColor rounded-positionbtn relation absolute bottom-[-2041px] left-[530px]'>
+                    <div className='w-[750px] h-[250px] bg-recColor rounded-positionbtn relative absolute bottom-[-2041px] left-[530px]'>
                         <Flex>
-                            <div className={"border-r-2 border-black"}>
+                            <div className={"relative after:absolute after:top-0 after:right-0 after:content-[''] after:w-0.5 after:h-[160px] after:bg-black "}>
                                 <Heading name={"2010"} className={"font-podkova font-bold text-[64px]  pl-[70px] pt-[50px]"}></Heading>
                                 <Paragraph pText={"Founded"} className={"font-pops font-medium text-[28px] pl-[70px]"}></Paragraph>
                             </div>
